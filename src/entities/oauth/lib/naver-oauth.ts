ecf5060d60dc9c09ef7ed28b1f3de112f33db7c6
HyeonJunSong <music9801@naver.com>
2024-07-24 13:46:54 +0900

    #3 feat: naver oauth call
    naver get Token api call

---
+++ src/entities/oauth/lib/naver-oauth.ts
@@ -0,0 +1,39 @@
+import { NAVER_CLIENT_ID, NAVER_CLIENT_SECRET } from "@env";
+
+const NAVER_OAUTH_STATE = "test";
+
+async function getNaverToken(code: string) {
+  try {
+    return await fetch("https://nid.naver.com/oauth2.0/token", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        client_id: NAVER_CLIENT_ID,
+        client_secret: NAVER_CLIENT_SECRET,
+        code: code,
+        state: "test",
+      }).toString(),
+    })
+      .then((res) => {
+        switch (res.status) {
+          case 200:
+            return res.json();
+          case 401:
+            throw new Error("unauthorized");
+          case 500:
+            throw new Error("Internal Server Error");
+        }
+      })
+      .then((res) => {
+        return res.access_token;
+      });
+  } catch (e) {
+    console.error(e);
+  }
+  return "";
+}
+
+export { getNaverToken, NAVER_OAUTH_STATE };
