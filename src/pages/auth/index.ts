42019df2ad2c3338ae747acb20f560a672d73310
HyeonJunSong <music9801@naver.com>
2024-07-24 13:52:04 +0900

    #3 shared: index files

---
+++ src/pages/auth/index.ts
@@ -0,0 +1,5 @@
+export * from "./ui/auth-page";
+export * from "./ui/kakao-auth-page";
+export * from "./ui/google-auth-page";
+export * from "./ui/naver-auth-page";
+export * from "./ui/apple-auth-page";
