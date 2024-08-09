beeb12e2228881aa47de8e4f99f82850575d214a
HyeonJunSong <music9801@naver.com>
2024-08-09 09:47:52 +0900

    #13 feat: add splash screen logic

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,23 +1,32 @@
-import { useUserStore } from '@/src/features';
+import { useCoupleStore, useUserStore } from '@/src/features';
 import { NavProp } from '@/src/shared';
-import { useFocusEffect } from '@react-navigation/native';
-import { useEffect } from 'react';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { useCallback, useEffect } from 'react';
 import { Text, View } from 'react-native';
 
-function SplashPage({ navigation }: { navigation: NavProp<'splash/index'> }) {
-  const { id, requiredTermsAgreed, birth, name, getUserInfo } = useUserStore();
+function SplashPage() {
+  const { requiredTermsAgreed, birth, name, getUserInfo, role } = useUserStore();
+  const { getCoupleInfo } = useCoupleStore();
+  const navigation = useNavigation<NavProp<'splash/index'>>();
 
-  useFocusEffect(() => {
-    getUserInfo().then((res) => {
-      if (!res) navigation.navigate('auth/index', { platform: null, accessToken: null });
-    });
-  });
+  useFocusEffect(
+    useCallback(() => {
+      getUserInfo().then((res) => {
+        if (!res) navigation.navigate('auth/index', { platform: null, accessToken: null });
+      });
+    }, [])
+  );
 
   useEffect(() => {
     if (!requiredTermsAgreed) navigation.navigate('terms-of-service/index');
-    else if (!birth || !name) navigation.navigate('info-setup/index');
-    else navigation.navigate('main/index');
-  }, [id]);
+    else if (birth === null || name === null) navigation.navigate('info-setup/index');
+    else if (role === 'ROLE_COUPLE') {
+      getCoupleInfo().then((res) => {
+        if (!res) navigation.navigate('auth/index', { platform: null, accessToken: null });
+        else navigation.navigate('main/index');
+      });
+    } else navigation.navigate('main/index');
+  }, [role]);
 
   return (
     <View>
@@ -26,4 +35,12 @@ function SplashPage({ navigation }: { navigation: NavProp<'splash/index'> }) {
   );
 }
 
-export { SplashPage };
+function SplashPageBeforeLoaded() {
+  return (
+    <View>
+      <Text>Splash</Text>
+    </View>
+  );
+}
+
+export { SplashPage, SplashPageBeforeLoaded };
