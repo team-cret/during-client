91880d34963b239d0fb8f667b33afdcb5a908084
HyeonJunSong <music9801@naver.com>
2024-09-01 20:22:32 +0900

    #21 feat: decorate purchase state management

--- src/features/decoration/store/decorate-avatar-store.ts
+++ src/features/decoration/store/decorate-avatar-store.ts
@@ -1,19 +1,38 @@
-import { avatarDecorationCategories, avatarDecorationCategoriesType } from '@/src/shared';
+import {
+  avatarDecorationCategories,
+  avatarDecorationCategoriesType,
+  AvatarItem,
+  avatarItems,
+} from '@/src/shared';
 import { create } from 'zustand';
 
 type State = {
   mode: number; // 0 : shop, 1 : bag
   category: avatarDecorationCategoriesType;
+  isPurchaseMode: boolean;
+  purchaseItems: Array<{
+    item: AvatarItem;
+    isSelected: boolean;
+  }>;
+  bagItems: Array<AvatarItem>;
+  shopItems: Array<AvatarItem>;
 };
 
 const defaultState: State = {
   mode: 1,
   category: avatarDecorationCategories[0],
+  isPurchaseMode: false,
+  purchaseItems: [],
+  bagItems: [avatarItems[0]],
+  shopItems: [avatarItems[0]],
 };
 
 type Action = {
   setMode: (mode: number) => void;
   setCategory: (category: avatarDecorationCategoriesType) => void;
+  setIsPurchaseMode: (isPurchaseMode: boolean) => void;
+  selectShopItem: (id: number) => void;
+  togglePurchaseItem: (id: number) => void;
 };
 
 const useDecorateAvatarStore = create<State & Action>((set) => ({
@@ -22,6 +41,23 @@ const useDecorateAvatarStore = create<State & Action>((set) => ({
   //actions
   setMode: (mode) => set({ mode }),
   setCategory: (category) => set({ category }),
+  setIsPurchaseMode: (isPurchaseMode) => set({ isPurchaseMode }),
+  selectShopItem: (id) => {
+    const selectedItem = avatarItems.find((item) => item.id === id);
+    if (!selectedItem) return;
+    set((state) => ({
+      ...state,
+      purchaseItems: [...state.purchaseItems, { item: selectedItem, isSelected: true }],
+    }));
+  },
+  togglePurchaseItem: (id) => {
+    set((state) => ({
+      ...state,
+      purchaseItems: state.purchaseItems.map((item) =>
+        item.item.id === id ? { ...item, isSelected: !item.isSelected } : item
+      ),
+    }));
+  },
 }));
 
 export { useDecorateAvatarStore };
--- src/features/decoration/store/decorate-room-store.ts
+++ src/features/decoration/store/decorate-room-store.ts
@@ -1,27 +1,63 @@
-import { roomDecorationCategories, roomDecorationCategoriesType } from '@/src/shared';
+import {
+  roomDecorationCategories,
+  roomDecorationCategoriesType,
+  RoomItem,
+  roomItems,
+} from '@/src/shared';
 import { create } from 'zustand';
 
 type State = {
   mode: number; // 0 : shop, 1 : bag
   category: roomDecorationCategoriesType;
+  isPurchaseMode: boolean;
+  purchaseItems: Array<{
+    item: RoomItem;
+    isSelected: boolean;
+  }>;
+  bagItems: Array<RoomItem>;
+  shopItems: Array<RoomItem>;
 };
 
 const defaultState: State = {
   mode: 1,
   category: roomDecorationCategories[0],
+  isPurchaseMode: false,
+  purchaseItems: [],
+  bagItems: [roomItems[0], roomItems[1], roomItems[2], roomItems[3], roomItems[4], roomItems[5]],
+  shopItems: [roomItems[0], roomItems[1], roomItems[2], roomItems[3]],
 };
 
 type Action = {
   setMode: (mode: number) => void;
   setCategory: (category: roomDecorationCategoriesType) => void;
+  setIsPurchaseMode: (isPurchaseMode: boolean) => void;
+  selectShopItem: (id: number) => void;
+  togglePurchaseItem: (id: number) => void;
 };
 
-const useDecorateRoomStore = create<State & Action>((set) => ({
+const useDecorateRoomStore = create<State & Action>((set, get) => ({
   ...defaultState,
 
   //actions
   setMode: (mode) => set({ mode }),
   setCategory: (category) => set({ category }),
+  setIsPurchaseMode: (isPurchaseMode) => set({ isPurchaseMode }),
+  selectShopItem: (id) => {
+    const selectedItem = get().shopItems.find((item) => item.id === id);
+    if (!selectedItem) return;
+    set((state) => ({
+      ...state,
+      purchaseItems: [...state.purchaseItems, { item: selectedItem, isSelected: true }],
+    }));
+  },
+  togglePurchaseItem: (id) => {
+    set((state) => ({
+      ...state,
+      purchaseItems: state.purchaseItems.map((item) =>
+        item.item.id === id ? { ...item, isSelected: !item.isSelected } : item
+      ),
+    }));
+  },
 }));
 
 export { useDecorateRoomStore };
