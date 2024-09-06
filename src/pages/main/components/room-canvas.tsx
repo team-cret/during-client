ea772d5889e3a90983055484502cbba26c7e98db
HyeonJunSong <music9801@naver.com>
2024-09-06 15:40:01 +0900

    #23 feat: avatar added to main page

--- src/pages/main/components/room-canvas.tsx
+++ src/pages/main/components/room-canvas.tsx
@@ -1,12 +1,20 @@
 import { useRoomStore } from '@/src/features';
-import { COLOR_BACKGROUND, convertHeight, convertWidth } from '@/src/shared';
-import { Gltf } from '@react-three/drei/native';
+import {
+  avatarDecorationCategoriesType,
+  AvatarItem,
+  AvatarObject,
+  COLOR_BACKGROUND,
+  convertHeight,
+  convertWidth,
+} from '@/src/shared';
+import { Gltf, useGLTF } from '@react-three/drei/native';
 import { Canvas } from '@react-three/fiber/native';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import THREE from 'three';
 
 function RoomCanvas() {
+  const { myAvatar } = useRoomStore();
   return (
     <View style={styles.container}>
       <Canvas
@@ -20,10 +28,19 @@ function RoomCanvas() {
       >
         <color attach="background" args={[COLOR_BACKGROUND]} />
         <Room />
+        <Avatar avatarInfo={myAvatar} />
       </Canvas>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: convertWidth(375),
+    height: convertHeight(700),
+    position: 'absolute',
+  },
+});
 
 function Room() {
   const { background, objects } = useRoomStore();
@@ -57,12 +74,66 @@ function Room() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    width: convertWidth(375),
-    height: convertHeight(700),
-    position: 'absolute',
-  },
-});
+function Avatar({
+  avatarInfo,
+}: {
+  avatarInfo: {
+    style: {
+      [key in avatarDecorationCategoriesType]: AvatarItem | null;
+    };
+    position: THREE.Vector3 | null;
+    rotation: number;
+    animation: number;
+  };
+}) {
+  const model = useGLTF(AvatarObject.avatar.src);
+
+  // const animations = useAnimations(model.animations, model.scene);
+  // useFocusEffect(
+  //   useCallback(() => {
+  //     animations.actions['Idle']?.play();
+  //   }, [])
+  // );
+
+  useEffect(() => {
+    model.scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.visible = false;
+        if (avatarInfo.style.상의 === null && child.name === 'basic-tops') child.visible = true;
+        if (avatarInfo.style.하의 === null && child.name === 'basic-bottoms') child.visible = true;
+        if (child.name === 'body') child.visible = true;
+
+        if (avatarInfo.style.상의 && child.name === avatarInfo.style.상의.name)
+          child.visible = true;
+        if (avatarInfo.style.하의 && child.name === avatarInfo.style.하의.name)
+          child.visible = true;
+        if (avatarInfo.style.신발 && child.name === avatarInfo.style.신발.name)
+          child.visible = true;
+        if (avatarInfo.style.헤어 && child.name === avatarInfo.style.헤어.name)
+          child.visible = true;
+      }
+    });
+  }, [avatarInfo.style]);
+
+  useEffect(() => {
+    if (avatarInfo.position) {
+      model.scene.position.set(
+        1 + avatarInfo.position.x,
+        avatarInfo.position.y,
+        1 + avatarInfo.position.z
+      );
+    }
+  }, [avatarInfo.position]);
+
+  useEffect(() => {
+    model.scene.rotation.y = (avatarInfo.rotation * Math.PI) / 2;
+  }, [avatarInfo.rotation]);
+
+  return (
+    <Suspense>
+      <primitive object={model.scene} />
+    </Suspense>
+  );
+}
 
 export { RoomCanvas };
