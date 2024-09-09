bb1fb76ba7c86f3dfab97bb247877330a4401b5d
HyeonJunSong <music9801@naver.com>
2024-09-09 17:09:15 +0900

    #23 feat: added init room function

--- src/features/home/store/room-store.ts
+++ src/features/home/store/room-store.ts
@@ -15,6 +15,7 @@ type State = {
     position: THREE.Vector3;
     rotation: number;
   }>;
+  isObjectExists: Array<Array<boolean>>;
 
   myAvatar: {
     style: {
@@ -36,18 +37,8 @@ type State = {
 
 const defaultState: State = {
   background: roomItems[0],
-  objects: [
-    {
-      item: roomItems[1],
-      position: new THREE.Vector3(0, 0, 0),
-      rotation: 0,
-    },
-    {
-      item: roomItems[3],
-      position: new THREE.Vector3(7, 0, 5),
-      rotation: 0,
-    },
-  ],
+  objects: [],
+  isObjectExists: Array.from({ length: 12 }, () => Array.from({ length: 12 }, () => false)),
   myAvatar: {
     style: {
       헤어: null,
@@ -55,8 +46,8 @@ const defaultState: State = {
       하의: null,
       신발: null,
     },
-    position: new THREE.Vector3(8, 0, 7),
-    rotation: 0.5,
+    position: new THREE.Vector3(-1, -1, -1),
+    rotation: 0,
     animation: 0,
   },
   otherAvatar: {
@@ -66,13 +57,14 @@ const defaultState: State = {
       하의: null,
       신발: null,
     },
-    position: new THREE.Vector3(8, 0, 7),
+    position: new THREE.Vector3(-1, -1, -1),
     rotation: 0,
     animation: 0,
   },
 };
 
 type Action = {
+  initRoom: () => void;
   updateMyAvatarStyle: ({
     style,
   }: {
@@ -91,12 +83,56 @@ type Action = {
       rotation: number;
     }>;
   }) => void;
+  updateMyAvatarPosition: (position: THREE.Vector3) => void;
 };
 
-const useRoomStore = create<State & Action>((set) => ({
+const useRoomStore = create<State & Action>((set, get) => ({
   ...defaultState,
 
   //actions
+  initRoom: () => {
+    const objects = [
+      {
+        item: roomItems[1],
+        position: new THREE.Vector3(0, 0, 0),
+        rotation: 0,
+      },
+      {
+        item: roomItems[3],
+        position: new THREE.Vector3(7, 0, 5),
+        rotation: 0,
+      },
+    ];
+    const isObjectExists = Array.from({ length: 12 }, () =>
+      Array.from({ length: 12 }, () => false)
+    );
+    for (const object of objects) {
+      for (
+        let i = 0;
+        i < (object.rotation % 2 === 0 ? object.item.size.width : object.item.size.depth);
+        i++
+      ) {
+        for (
+          let j = 0;
+          j < (object.rotation % 2 === 1 ? object.item.size.width : object.item.size.depth);
+          j++
+        ) {
+          isObjectExists[object.position.z + i][object.position.x + j] = true;
+        }
+      }
+    }
+
+    set({
+      ...defaultState,
+      objects,
+      isObjectExists,
+      myAvatar: {
+        ...defaultState.myAvatar,
+        position: new THREE.Vector3(8, 0, 8),
+        rotation: 0,
+      },
+    });
+  },
   updateMyAvatarStyle: ({ style }) => {
     set((state) => ({
       ...state,
@@ -107,10 +143,53 @@ const useRoomStore = create<State & Action>((set) => ({
     }));
   },
   updateRoom: ({ background, objects }) => {
+    const newIsObjectExists = Array.from({ length: 12 }, () =>
+      Array.from({ length: 12 }, () => false)
+    );
+    for (const object of objects) {
+      for (
+        let i = 0;
+        i < (object.rotation % 2 === 0 ? object.item.size.width : object.item.size.depth);
+        i++
+      ) {
+        for (
+          let j = 0;
+          j < (object.rotation % 2 === 1 ? object.item.size.width : object.item.size.depth);
+          j++
+        ) {
+          newIsObjectExists[object.position.z + i][object.position.x + j] = true;
+        }
+      }
+    }
+
     set((state) => ({
       ...state,
       background: background !== null ? background : state.background,
+      isObjectExists: newIsObjectExists,
       objects,
+      myAvatar: {
+        ...state.myAvatar,
+        route: [],
+      },
+      otherAvatar: {
+        ...state.otherAvatar,
+        route: [],
+      },
+    }));
+  },
+  updateMyAvatarPosition: (position) => {
+    position = new THREE.Vector3(
+      Math.min(Math.floor(position.x), 11),
+      0,
+      Math.min(Math.floor(position.z), 11)
+    );
+
+    set((state) => ({
+      ...state,
+      myAvatar: {
+        ...state.myAvatar,
+        position,
+      },
     }));
   },
 }));
