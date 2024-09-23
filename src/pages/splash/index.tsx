8ea1ac0229d5cbc47ecaa712df141f1804c7e187
HyeonJunSong <music9801@naver.com>
2024-09-23 15:48:33 +0900

    #29 feat: logo application

--- src/pages/splash/index.tsx
+++ src/pages/splash/index.tsx
@@ -1,10 +1,11 @@
 import { setMemberPushTokenAPI } from '@/src/entities';
 import { useCoupleStore, useUserStore } from '@/src/features';
-import { NavProp } from '@/src/shared';
+import { COLOR_BACKGROUND, NavProp } from '@/src/shared';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { useCallback } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { registerForPushNotificationsAsync } from '../notification-test';
+import LongLogoIcon from '@/src/shared/assets/icons/logo/logo-long.svg';
 
 function SplashPage() {
   const { requiredTermsAgreed, birth, name, getUserInfo, role, id } = useUserStore();
@@ -41,18 +42,27 @@ function SplashPage() {
   );
 
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <LongLogoIcon />
     </View>
   );
 }
 
 function SplashPageBeforeLoaded() {
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <LongLogoIcon />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLOR_BACKGROUND,
+  },
+});
 
 export { SplashPage, SplashPageBeforeLoaded };
