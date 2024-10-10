91a59d8b29ec8afff34c5047f1b153aaf78d9499
HyeonJunSong <music9801@naver.com>
2024-10-10 15:01:38 +0900

    #31 bugfix: missing backward logics

--- src/pages/decorate-avatar/index.tsx
+++ src/pages/decorate-avatar/index.tsx
@@ -1,16 +1,23 @@
-import { COLOR_BACKGROUND, convertHeight, convertWidth } from '@/src/shared';
+import { COLOR_BACKGROUND, convertHeight, convertWidth, NavProp } from '@/src/shared';
 import { TitleCloseAppbar } from '@/src/widgets';
 import { StyleSheet, View } from 'react-native';
 import { UpperButtons } from './components/upper-buttons';
 import { BottomSection } from './components/bottom-section';
 import { PurchaseBottomSheet } from './components/purchase-bottom-sheet';
 import { AvatarCanvas } from './components/avatar-canvas';
+import { useNavigation } from 'expo-router';
 
 function DecorateAvatarPage() {
+  const navigation = useNavigation<NavProp<'decorate-avatar/index'>>();
   return (
     <View style={styles.container}>
       <AvatarCanvas />
-      <TitleCloseAppbar title="아바타 꾸미기" />
+      <TitleCloseAppbar
+        title="아바타 꾸미기"
+        onPress={() => {
+          navigation.pop();
+        }}
+      />
       <UpperButtons />
       <BottomSection />
       <PurchaseBottomSheet />
